b70ff903900fd79be7240c7f6bb2a17fb801a83b
tfack <tfackenthall@yahoo.com>
2022-05-02 15:42:24 -0700

    add db

--- app.py
+++ app.py
@@ -3,20 +3,42 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 from yelp import find_coffee
+from flask_login import current_user, login_user, login_required, logout_user
+from models import db, login, UserModel
 
 class loginForm(FlaskForm):
     email=StringField(label="Enter email", validators=[DataRequired(),Email()])
     password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=6,max=16)])
     submit=SubmitField(label="Login")
 
-passwords={}
-passwords['tos@uw.edu']='qwerty'
+#passwords={}
+#passwords['tos@uw.edu']='qwerty'
 
 app = Flask(__name__)
 app.secret_key="a secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/login.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+login.init_app(app)
+
+def addUser(email, password):
+    user=UserModel()
+    user.set_password(password)
+    user.email = email
+    db.session.add(user)
+    db.session.commit()
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+    user = UserModel.query.filter_by(email = 'tos@uw.edu').first()
+    if user is None:
+        addUser('tos@uw.edu', 'qwerty')
 
 @app.route("/home")
-def helloworld():
+@login_required
+def findCoffee():
     return render_template("home.html", myData=find_coffee())
 
 @app.route("/")
@@ -28,11 +50,18 @@ def login():
     form=loginForm()
     if form.validate_on_submit():
         if request.method == "POST":
-            user=request.form["email"]
+            email=request.form["email"]
             pw=request.form["password"]
-            if user is not None and user in passwords and passwords[user] == pw :
+            user = UserModel.query.filter_by(email = email).first()
+            if email is not None and user.check_password(pw):
+                login_user(user)
                 return redirect('/home')
     return render_template("login.html",form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/login')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
